4f5a1993b382b3e59fc6b2187e2b0bee5e8f479a
JohannesStroebele <stroebele.j@gmail.com>
2020-06-02 20:52:59 +0200

    Added app.yaml and requirements.txt for deployment

--- frontend/main.py
+++ frontend/main.py
@@ -1,9 +1,13 @@
+import os
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
 from frontend.pages import page_overview, page_controlling, page_downtimes, page_vehiclestables
+
+server = app.server
 
 app = dash.Dash()
 
@@ -61,7 +65,7 @@ def display_page(pathname):
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True, host='0.0.0.0')
+    app.server(host='0.0.0.0', port=8080, debug=True)
 
 app.css.config.serve_locally = True
 app.scripts.config.serve_locally = True
