71d422003a6b049e0fd3993ef27a75b95c801e25
luciejuergens <lj025@hdm-stuttgart.de>
2020-06-03 15:21:51 +0200

    - import dash added - first table added

--- frontend/pages/page_vehiclestables.py
+++ frontend/pages/page_vehiclestables.py
@@ -2,8 +2,9 @@ import dash_core_components as dcc
 import dash_html_components as html
 import dash_table
 import pandas as pd
+import dash
 
-fleet_data = pd.read_csv('../batch-data/cleaned-data-for-fleet-dna.csv')
+fleet_data = pd.read_csv('../../batch-data/cleaned-data-for-fleet-dna.csv')
 
 page_layout = html.Div([
     html.H1('Vehicles Tables'),
@@ -36,3 +37,33 @@ page_layout = html.Div([
             }
         ])
 ])
+
+
+df = pd.read_csv('../../batch-data/cleaned-data-for-fleet-dna.csv')
+df_vehicle_table1 = df[['vehicel_type','drivetrain_type','fuel_type','day_id','speed_data_duration_hrs_includes_zero']]
+
+
+def generate_table(dataframe, max_rows=10):
+    return html.Table([
+        html.Thead(
+            html.Tr([html.Th(col) for col in dataframe.columns])
+        ),
+        html.Tbody([
+            html.Tr([
+                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+            ]) for i in range(min(len(dataframe), max_rows))
+        ])
+    ])
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(children=[
+    html.H4(children='Vehicle Table'),
+    generate_table(df_vehicle_table1)
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
