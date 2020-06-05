32112601b601295633f8b3e943d75485af450eba
jakobschaal <js325@hdm-stuttgart.de>
2020-06-05 02:56:48 +0200

    downtimes changes

--- frontend/pages/downtimes.py
+++ frontend/pages/downtimes.py
@@ -2,14 +2,92 @@ import dash_core_components as dcc
 import dash_html_components as html
 import dash_table
 import pandas as pd
+import plotly.graph_objects as go
+
+
+#Mapbox
+#mapbox_access_token = open("pk.eyJ1IjoiamFrb2JzY2hhYWwiLCJhIjoiY2tiMWVqYnYwMDEyNDJ5bWF3YWhnMTFnNCJ9.KitYnq2a645C15FwvFdqAw").read()
+
+fig = go.Figure(go.Scattermapbox(
+        lat=['38.91427','38.91538','38.91458',
+             '38.92239','38.93222','38.90842',
+             '38.91931','38.93260','38.91368',
+             '38.88516','38.921894','38.93206',
+             '38.91275'],
+        lon=['-77.02827','-77.02013','-77.03155',
+             '-77.04227','-77.02854','-77.02419',
+             '-77.02518','-77.03304','-77.04509',
+             '-76.99656','-77.042438','-77.02821',
+             '-77.01239'],
+        mode='markers',
+        marker=go.scattermapbox.Marker(
+            size=9
+        ),
+        text=["Truck 1","Truck 2","Truck 3",
+             "Truck 4","Truck 5","Truck 6",
+             "Truck 7","Truck 8","Truck 9",
+             "Truck 10","Truck 11","Truck 12",
+             "Truck 13"],
+    ))
+
+fig.update_layout(
+    autosize=True,
+    hovermode='closest',
+    mapbox=dict(
+        accesstoken='pk.eyJ1IjoiamFrb2JzY2hhYWwiLCJhIjoiY2tiMWVqYnYwMDEyNDJ5bWF3YWhnMTFnNCJ9.KitYnq2a645C15FwvFdqAw',
+        bearing=0,
+        center=dict(
+            lat=38.92,
+            lon=-77.07
+        ),
+        pitch=0,
+        zoom=10
+    ),
+)
+
+
+
+
+#Daten
+
 
 fleet_data = pd.read_csv('../batch-data/cleaned-data-for-fleet-dna.csv')
 fleet_data = fleet_data.head(10) # limits the displayed rows to 10
 
 page_layout = html.Div([
-    html.H1('Downtimes'),
-    html.Br(),
-    dash_table.DataTable(
+
+#Tab-Layout
+
+    dcc.Tabs([
+        dcc.Tab(label='Downtimes', children=[
+            dcc.Graph(
+                figure={
+                    'data': [
+                        {'x': [1, 2, 3], 'y': [4, 1, 2],
+                         'type': 'bar', 'name': 'SF'},
+                        {'x': [1, 2, 3], 'y': [2, 4, 5],
+                         'type': 'bar', 'name': u'Montréal'},
+                    ]
+                }
+            )
+        ]),
+        dcc.Tab(label='Maintenance', children=[
+            dcc.Graph(
+                figure={
+                    'data': [
+                        {'x': [1, 2, 3], 'y': [1, 4, 1],
+                         'type': 'bar', 'name': 'SF'},
+                        {'x': [1, 2, 3], 'y': [1, 2, 3],
+                         'type': 'bar', 'name': u'Montréal'},
+                    ]
+                }
+            )
+        ]),
+        dcc.Tab(label='Map', children=[
+
+            dcc.Graph(figure=fig),
+
+        dash_table.DataTable(
         data=fleet_data.to_dict('records'),
         columns=[{'id': c, 'name': c} for c in fleet_data.columns],
         style_cell={'textAlign': 'left'},
@@ -19,4 +97,13 @@ page_layout = html.Div([
                 'textAlign': 'left'
             }
         ])
+
+
+
+
+
+
+
+        ]),
+    ])
 ])
