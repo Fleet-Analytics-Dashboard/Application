81f9b4e51913d0d8e4869e9abb2c47fa8c9bf4da
jakobschaal <js325@hdm-stuttgart.de>
2020-06-08 02:01:35 +0200

    nav changes

--- frontend/main.py
+++ frontend/main.py
@@ -2,27 +2,41 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
 
 from frontend.pages import overview, controlling, downtimes, vehiclestables
 
-app = dash.Dash()
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+#Navigationbar
+
+navigation = dbc.Nav(
+    [
+        dbc.NavItem(dbc.NavLink("Overview", active=True, href="/")),
+        dbc.NavItem(dbc.NavLink("Controlling", href="/controlling")),
+        dbc.NavItem(dbc.NavLink("Downtimes", href="/downtimes")),
+        dbc.NavItem(dbc.NavLink("Vehicle Tables", href="/vehicles-tables")),
+    ],
+    pills=True,
+)
 
 app.config.suppress_callback_exceptions = True
 
 app.layout = html.Div([
+    navigation,
     # represents the URL bar, doesn't render anything
     dcc.Location(id='url', refresh=False),
-    html.H1('Navigation'),
-    html.Br(),
-    dcc.Link('Overview', href='/'),
-    html.Br(),
-    dcc.Link('Controlling ', href='/controlling'),
-    html.Br(),
-    dcc.Link('Downtimes', href='/downtimes'),
-    html.Br(),
-    dcc.Link('Vehicles tables', href='/vehicles-tables'),
-    html.Br(),
-    html.Br(),
+    #html.H1('Navigation'),
+    #html.Br(),
+    #dcc.Link('Overview', href='/'),
+    #html.Br(),
+    #dcc.Link('Controlling ', href='/controlling'),
+    #html.Br(),
+    #dcc.Link('Downtimes', href='/downtimes'),
+    #html.Br(),
+    #dcc.Link('Vehicles tables', href='/vehicles-tables'),
+    #html.Br(),
+    #html.Br(),
     # page content from respective site will be loaded via this id
     html.Div(id='page-content')
 ])
