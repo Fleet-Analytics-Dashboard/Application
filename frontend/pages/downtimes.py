a80fe5718fe708c07faae3a82f4ce86153881652
Jakob <js325@hdm-stuttgart.de>
2020-06-18 16:51:41 +0200

    update branch

--- frontend/pages/downtimes.py
+++ frontend/pages/downtimes.py
@@ -3,62 +3,63 @@ import dash_html_components as html
 import dash_table
 import pandas as pd
 import plotly.graph_objects as go
+import calendar
+import matplotlib.pyplot as plt
 
-
-#Daten
+# Daten
 
 fleet_data = pd.read_csv('../batch-data/cleaned-data-for-fleet-dna.csv')
-fleet_data = fleet_data.head(10) # limits the displayed rows to 10
-#fleet_data.iloc[:,1:3]
+fleet_data = fleet_data.head(10)  # limits the displayed rows to 10
+# fleet_data.iloc[:,1:3]
 
 
-#PieCharts
+# PieCharts
 
-#Downtimes Overview
+# Downtimes Overview
 
-labels = ['Accidents','Traffic Jams','Maintenance','Unused']
+labels = ['Accidents', 'Traffic Jams', 'Maintenance', 'Unused']
 values = [20, 30, 10, 40]
 
 pie1 = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.3)])
 
-#Need for Maintenance
+# Need for Maintenance
 
-labels = ['Need','Soon','No need']
+labels = ['Need', 'Soon', 'No need']
 values = [2, 5, 10]
 
 pie2 = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.3)])
 
-#Accident Probability
+# Accident Probability
 
-labels = ['Category 1','Category 2','Category 3']
+labels = ['Category 1', 'Category 2', 'Category 3']
 values = [20, 30, 10, 40]
 
 pie3 = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.3)])
 
-#Mapbox
-#mapbox_access_token = open(".mapbox_token").read()
+# Mapbox
+# mapbox_access_token = open(".mapbox_token").read()
 
 fig = go.Figure(go.Scattermapbox(
-        lat=['38.91427','38.91538','38.91458',
-             '38.92239','38.93222','38.90842',
-             '38.91931','38.93260','38.91368',
-             '38.88516','38.921894','38.93206',
-             '38.91275'],
-        lon=['-77.02827','-77.02013','-77.03155',
-             '-77.04227','-77.02854','-77.02419',
-             '-77.02518','-77.03304','-77.04509',
-             '-76.99656','-77.042438','-77.02821',
-             '-77.01239'],
-        mode='markers',
-        marker=go.scattermapbox.Marker(
-            size=9
-        ),
-        text=["Truck 1","Truck 2","Truck 3",
-             "Truck 4","Truck 5","Truck 6",
-             "Truck 7","Truck 8","Truck 9",
-             "Truck 10","Truck 11","Truck 12",
-             "Truck 13"],
-    ))
+    lat=['38.91427', '38.91538', '38.91458',
+         '38.92239', '38.93222', '38.90842',
+         '38.91931', '38.93260', '38.91368',
+         '38.88516', '38.921894', '38.93206',
+         '38.91275'],
+    lon=['-77.02827', '-77.02013', '-77.03155',
+         '-77.04227', '-77.02854', '-77.02419',
+         '-77.02518', '-77.03304', '-77.04509',
+         '-76.99656', '-77.042438', '-77.02821',
+         '-77.01239'],
+    mode='markers',
+    marker=go.scattermapbox.Marker(
+        size=9
+    ),
+    text=["Truck 1", "Truck 2", "Truck 3",
+          "Truck 4", "Truck 5", "Truck 6",
+          "Truck 7", "Truck 8", "Truck 9",
+          "Truck 10", "Truck 11", "Truck 12",
+          "Truck 13"],
+))
 
 fig.update_layout(
     autosize=True,
@@ -76,6 +77,73 @@ fig.update_layout(
     ),
 )
 
+###Kalender###
+
+calendar.setfirstweekday(0)
+w_days = 'Sun Mon Tue Wed Thu Fri Sat'.split()
+m_names = '''
+January February March April
+May June July August
+September October November December'''.split()
+
+
+class MplCalendar(object):
+
+    def __init__(self, year, month):
+        self.year = year
+        self.month = month
+        self.cal = calendar.monthcalendar(year, month)
+        # monthcalendar creates a list of lists for each week
+        # Save the events data in the same format
+        self.events = [[[] for day in week] for week in self.cal]
+
+
+    def _monthday_to_index(self, day):
+        for week_n, week in enumerate(self.cal):
+            try:
+                i = week.index(day)
+                return week_n, i
+            except ValueError:
+                pass
+        # couldn't find the day
+        raise ValueError("There aren't {} days in the month".format(day))
+
+
+    def add_event(self, day, event_str):
+        week, w_day = self._monthday_to_index(day)
+        self.events[week][w_day].append(event_str)
+
+    def show(self):
+        ##create calendar
+        f, axs = plt.subplots(len(self.cal), 7, sharex=True, sharey=True)
+        for week, ax_row in enumerate(axs):
+            for week_day, ax in enumerate(ax_row):
+                ax.set_xticks([])
+                ax.set_yticks([])
+                if self.cal[week][week_day] != 0:
+                    ax.text(.02, .98,
+                            str(self.cal[week][week_day]),
+                            verticalalignment='top',
+                            horizontalalignment='left')
+                contents = "\n".join(self.events[week][week_day])
+                ax.text(.03, .85, contents,
+                        verticalalignment='top',
+                        horizontalalignment='left',
+                        fontsize=9)
+
+        # use the titles of the first row as the weekdays
+        for n, day in enumerate(w_days):
+            axs[0][n].set_title(day)
+
+        # Place subplots in a close grid
+        f.subplots_adjust(hspace=0)
+        f.subplots_adjust(wspace=0)
+        f.suptitle(m_names[self.month] + ' ' + str(self.year),
+                   fontsize=20, fontweight='bold')
+    plt.show()
+
+
+plt = go.Figure(plt.show())
 
 
 
@@ -83,65 +151,63 @@ fig.update_layout(
 
 page_layout = html.Div([
 
-#Tab-Layout
+    # Tab-Layout
 
     dcc.Tabs([
 
-#Downtimes View
+        # Downtimes View
 
         dcc.Tab(label='Downtimes', children=[
-        #html.H2('Downtimes'),
-#Row 1
-        html.Div([
+            # html.H2('Downtimes'),
+            # Row 1
+            html.Div([
 
                 html.Div([
                     html.Div(
                         html.H2('Vehicle Downtimes'),
                         style={'text-align': 'center'}
-                        ),
+                    ),
                     html.Div([
-                        #Graph Towntimes
+                        # Graph Downtimes
                         dcc.Graph(figure=pie1, style={'width': '59%', 'margin': '0'}),
 
-                       # dcc.Graph(figure=pie1, style={'width': '40%', 'margin': '0'}),
+                        # dcc.Graph(figure=pie1, style={'width': '40%', 'margin': '0'}),
                     ]),
-                ],style={'width': '49%', 'display': 'inline-block', 'margin': '0'}),
+                ], style={'width': '49%', 'display': 'inline-block', 'margin': '0'}),
 
-
-#Accidents
-            html.Div([
+                # Accidents
                 html.Div([
-                    html.Div(
-                        html.H2('Accidents'),
-                        style={'text-align': 'center'}
+                    html.Div([
+                        html.Div(
+                            html.H2('Accidents'),
+                            style={'text-align': 'center'}
                         ),
-                    html.Div(
-                        dcc.Graph(figure=fig),
+                        html.Div(
+                            dcc.Graph(figure=fig),
                         ),
-                ]),
+                    ]),
 
-            ],style={'width': '49%', 'display': 'inline-block', 'margin': '0'}),
-        ]),
+                ], style={'width': '49%', 'display': 'inline-block', 'margin': '0'}),
+            ]),
 
+            # Row 2
 
-#Row 2
-
-            #Need for Maintenance View
+            # Need for Maintenance View
             html.Div([
                 html.Div(
                     html.H2('Need for Maintanance'),
                     style={'text-align': 'center'}
                 ),
                 html.Div([
-                    #Graph - Need for Maintanance
+                    # Graph - Need for Maintanance
                     dcc.Graph(figure=pie1, style={'width': '59%', 'margin': '0'}),
 
-                    #Table - Need for maintenance
+                    # Table - Need for maintenance
 
                 ]),
             ], style={'width': '49%', 'display': 'inline-block'}),
 
-            #Accident probability view
+            # Accident probability view
             html.Div([
                 html.Div(
                     html.H2('Accident probability'),
@@ -156,106 +222,91 @@ page_layout = html.Div([
                 ]),
             ], style={'width': '49%', 'display': 'inline-block'}),
 
-#Row 3 - Truck / Driver information
+            # Row 3 - Truck / Driver information
 
-        #Overstepping speed limit table
-        html.Div(
-            dash_table.DataTable(
-            data=fleet_data.to_dict('records'),
-            #columns=[{'id': c, 'name': c} for c in fleet_data.columns],
-            columns=[{'name': i, 'id': i} for i in fleet_data.loc[:,['vid','vehicle_class']]],
-            style_cell={'textAlign': 'left'},
-            style_cell_conditional=[
+            # Overstepping speed limit table
+            html.Div(
+                dash_table.DataTable(
+                    data=fleet_data.to_dict('records'),
+                    # columns=[{'id': c, 'name': c} for c in fleet_data.columns],
+                    columns=[{'name': i, 'id': i} for i in fleet_data.loc[:, ['vid', 'vehicle_class']]],
+                    style_cell={'textAlign': 'left'},
+                    style_cell_conditional=[
 
-            ]),
-            style={'width': '20%', 'display': 'inline-block'}),
+                    ]),
+                style={'width': '20%', 'display': 'inline-block'}),
 
-        #Oldest Vehicles table
-        html.Div(
-            dash_table.DataTable(
-            data=fleet_data.to_dict('records'),
-            #columns=[{'id': c, 'name': c} for c in fleet_data.columns],
-            columns=[{'name': i, 'id': i} for i in fleet_data.loc[:,['vid','vehicle_class']]],
-            style_cell={'textAlign': 'left'},
-            style_cell_conditional=[
+            # Oldest Vehicles table
+            html.Div(
+                dash_table.DataTable(
+                    data=fleet_data.to_dict('records'),
+                    # columns=[{'id': c, 'name': c} for c in fleet_data.columns],
+                    columns=[{'name': i, 'id': i} for i in fleet_data.loc[:, ['vid', 'vehicle_class']]],
+                    style_cell={'textAlign': 'left'},
+                    style_cell_conditional=[
 
-            ]),
-            style={'width': '20%', 'display': 'inline-block'}),
+                    ]),
+                style={'width': '20%', 'display': 'inline-block'}),
 
-        #Excessive speeding table
-        html.Div(
-            dash_table.DataTable(
-            data=fleet_data.to_dict('records'),
-            #columns=[{'id': c, 'name': c} for c in fleet_data.columns],
-            columns=[{'name': i, 'id': i} for i in fleet_data.loc[:,['vid','vehicle_class']]],
-            style_cell={'textAlign': 'left'},
-            style_cell_conditional=[
+            # Excessive speeding table
+            html.Div(
+                dash_table.DataTable(
+                    data=fleet_data.to_dict('records'),
+                    # columns=[{'id': c, 'name': c} for c in fleet_data.columns],
+                    columns=[{'name': i, 'id': i} for i in fleet_data.loc[:, ['vid', 'vehicle_class']]],
+                    style_cell={'textAlign': 'left'},
+                    style_cell_conditional=[
 
-            ]),
-            style={'width': '20%', 'display': 'inline-block'}),
+                    ]),
+                style={'width': '20%', 'display': 'inline-block'}),
 
-        #Excessive acceleration table
-        html.Div(
-            dash_table.DataTable(
-            data=fleet_data.to_dict('records'),
-            #columns=[{'id': c, 'name': c} for c in fleet_data.columns],
-            columns=[{'name': i, 'id': i} for i in fleet_data.loc[:,['vid','vehicle_class']]],
-            style_cell={'textAlign': 'left'},
-            style_cell_conditional=[
+            # Excessive acceleration table
+            html.Div(
+                dash_table.DataTable(
+                    data=fleet_data.to_dict('records'),
+                    # columns=[{'id': c, 'name': c} for c in fleet_data.columns],
+                    columns=[{'name': i, 'id': i} for i in fleet_data.loc[:, ['vid', 'vehicle_class']]],
+                    style_cell={'textAlign': 'left'},
+                    style_cell_conditional=[
 
-            ]),
-            style={'width': '20%', 'display': 'inline-block'}),
+                    ]),
+                style={'width': '20%', 'display': 'inline-block'}),
 
-        #Excessive breaking table
-        html.Div(
-            dash_table.DataTable(
-            data=fleet_data.to_dict('records'),
-            #columns=[{'id': c, 'name': c} for c in fleet_data.columns],
-            columns=[{'name': i, 'id': i} for i in fleet_data.loc[:,['vid','vehicle_class']]],
-            style_cell={'textAlign': 'left'},
-            style_cell_conditional=[
+            # Excessive breaking table
+            html.Div(
+                dash_table.DataTable(
+                    data=fleet_data.to_dict('records'),
+                    # columns=[{'id': c, 'name': c} for c in fleet_data.columns],
+                    columns=[{'name': i, 'id': i} for i in fleet_data.loc[:, ['vid', 'vehicle_class']]],
+                    style_cell={'textAlign': 'left'},
+                    style_cell_conditional=[
 
-            ]),
-            style={'width': '20%', 'display': 'inline-block'}),
-
+                    ]),
+                style={'width': '20%', 'display': 'inline-block'}),
 
         ]),
 
-# Maintenance Calendar View
+        # Maintenance Calendar View
         dcc.Tab(label='Maintenance Calendar', children=[
-            dcc.Graph(
-                figure={
-                    'data': [
-                        {'x': [1, 2, 3], 'y': [1, 4, 1],
-                         'type': 'bar', 'name': 'SF'},
-                        {'x': [1, 2, 3], 'y': [1, 2, 3],
-                         'type': 'bar', 'name': u'Montréal'},
-                    ]
-                }
-            )
+            dcc.Graph(figure=plt)
+
         ]),
 
-        #Fleet location map view
+        # Fleet location map view
         dcc.Tab(label='Realtime Map', children=[
 
             dcc.Graph(figure=fig),
 
-        dash_table.DataTable(
-        data=fleet_data.to_dict('records'),
-        columns=[{'id': c, 'name': c} for c in fleet_data.columns],
-        style_cell={'textAlign': 'left'},
-        style_cell_conditional=[
-            {
-                'if': {'column_id': 'Region'},
-                'textAlign': 'left'
-            }
-        ])
-
-
-
-
-
-
+            dash_table.DataTable(
+                data=fleet_data.to_dict('records'),
+                columns=[{'id': c, 'name': c} for c in fleet_data.columns],
+                style_cell={'textAlign': 'left'},
+                style_cell_conditional=[
+                    {
+                        'if': {'column_id': 'Region'},
+                        'textAlign': 'left'
+                    }
+                ])
 
         ]),
     ])
