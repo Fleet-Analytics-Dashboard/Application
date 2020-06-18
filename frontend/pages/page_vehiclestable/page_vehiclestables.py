ae8376963f505c3cef8498a8278a9ebbb7fdb272
luciejuergens <lj025@hdm-stuttgart.de>
2020-06-18 17:37:20 +0200

    update

--- frontend/pages/page_vehiclestable/page_vehiclestables.py
+++ frontend/pages/page_vehiclestable/page_vehiclestables.py
@@ -4,59 +4,133 @@ import dash_html_components as html
 import dash_core_components as dcc
 import dash_table
 import pandas as pd
-from page_vehiclestable import table1
+from frontend.pages.page_vehiclestable import table1, table2
 
 
 app = dash.Dash()
 
-app.config['suppress_callback_exceptions'] = True
-
 app.layout = html.Div([
     html.H1('Dash Tabs component demo'),
-    dcc.Tabs(id="tabs-example", value='tab-1-example', children=[
-        dcc.Tab(label='Tab One', value='tab-1-example'),
-        dcc.Tab(label='Tab Two', value='tab-2-example'),
+    dcc.Tabs(id="tabs-example", value='tab-1', children=[
+        dcc.Tab(label='Tab One', value='tab-1'),
+        dcc.Tab(label='Tab Two', value='tab-2'),
     ]),
     html.Div(id='tabs-content-example')
 ])
-
-
-
-tab_2_layout = html.Div([
-    html.H1('Page 2'),
-    html.Div(dash_table.DataTable(
-            id='table-2',
-            data=fleet_data.to_dict('records'),
-            columns=[{'name': i, 'id': i} for i in df_vehicle_table1.loc[:,['vehicle_class']]
-                     ],
-            page_current=0,
-            page_size=20,
-            page_action='custom',
-
-            filter_action='custom',
-            filter_query='',
-
-            sort_action='custom',
-            sort_mode='multi',
-            sort_by=[],
-
-            style_cell={'textAlign': 'left'},
-            style_cell_conditional=[
-
-            ]),
-            style={'width': '20%', 'display': 'inline-block', 'overflowX': 'scroll'}),
-])
-
 
 #tab callback
 @app.callback(Output('tabs-content-example', 'children'),
               [Input('tabs-example', 'value')])
 def render_content(tab):
-    if tab == 'tab-1-example':
-        return tab_1_layout
-    elif tab == 'tab-2-example':
-        return tab_2_layout
+    if tab == 'tab-1':
+        return table1.tab_1_layout
+    elif tab == 'tab-2':
+        return table2.tab_2_layout
 
+#app callback table 1
+@app.callback(
+    Output('table-1', 'data'),
+    [Input('table-1', "page_current"),
+     Input('table-1', "page_size"),
+     Input('table-1', 'sort_by'),
+     Input('table-1', 'filter_query')])
+def update_table(page_current, page_size, sort_by, filter):
+    filtering_expressions = filter.split(' && ')
+    dff = df_vehicle_table1
+    for filter_part in filtering_expressions:
+        col_name, operator, filter_value = split_filter_part(filter_part)
+
+        if operator in ('eq', 'ne', 'lt', 'le', 'gt', 'ge'):
+            # these operators match pandas series operator method names
+            dff = dff.loc[getattr(dff[col_name], operator)(filter_value)]
+        elif operator == 'contains':
+            dff = dff.loc[dff[col_name].str.contains(filter_value)]
+        elif operator == 'datestartswith':
+            # this is a simplification of the front-end filtering logic,
+            # only works with complete fields in standard format
+            dff = dff.loc[dff[col_name].str.startswith(filter_value)]
+
+    if len(sort_by):
+        dff = dff.sort_values(
+            [col['column_id'] for col in sort_by],
+            ascending=[
+                col['direction'] == 'asc'
+                for col in sort_by
+            ],
+            inplace=False
+        )
+
+    page = page_current
+    size = page_size
+    return dff.iloc[page * size: (page + 1) * size].to_dict('records')
+
+
+@app.callback(
+    Output('table-paging-with-graph-container', "children"),
+    [Input('table-paging-with-graph', "data")])
+def update_graph(rows):
+    dff = pd.DataFrame(rows)
+    return html.Div(
+        [
+            dcc.Graph(
+                id=column,
+                figure={
+                    "data": [
+                        {
+                            "x": dff["vehicel_type"],
+                            "y": dff[column] if column in dff else [],
+                            "type": "bar",
+                            "marker": {"color": "#0074D9"},
+                        }
+                    ],
+                    "layout": {
+                        "xaxis": {"automargin": True},
+                        "yaxis": {"automargin": True},
+                        "height": 250,
+                        "margin": {"t": 10, "l": 10, "r": 10},
+                    },
+                },
+            )
+            for column in ["pop", "lifeExp", "gdpPercap"]
+        ]
+    )
+
+#app callback table 2
+@app.callback(
+    Output('table-2', 'data'),
+    [Input('table-2', "page_current"),
+     Input('table-2', "page_size"),
+     Input('table-2', 'sort_by'),
+     Input('table-2', 'filter_query')])
+def update_table(page_current, page_size, sort_by, filter):
+    filtering_expressions = filter.split(' && ')
+    dff = df_vehicle_table1
+    for filter_part in filtering_expressions:
+        col_name, operator, filter_value = split_filter_part(filter_part)
+
+        if operator in ('eq', 'ne', 'lt', 'le', 'gt', 'ge'):
+            # these operators match pandas series operator method names
+            dff = dff.loc[getattr(dff[col_name], operator)(filter_value)]
+        elif operator == 'contains':
+            dff = dff.loc[dff[col_name].str.contains(filter_value)]
+        elif operator == 'datestartswith':
+            # this is a simplification of the front-end filtering logic,
+            # only works with complete fields in standard format
+            dff = dff.loc[dff[col_name].str.startswith(filter_value)]
+
+    if len(sort_by):
+        dff = dff.sort_values(
+            [col['column_id'] for col in sort_by],
+            ascending=[
+                col['direction'] == 'asc'
+                for col in sort_by
+            ],
+            inplace=False
+        )
+
+    page = page_current
+    size = page_size
+    return dff.iloc[page * size: (page + 1) * size].to_dict('records')
 
 
 if __name__ == '__main__':
---
+++ frontend/pages/page_vehiclestable/table2.py
@@ -0,0 +1,72 @@
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_table
+import pandas as pd
+import dash
+
+#data
+fleet_data = pd.read_csv('../../../batch-data/cleaned-data-for-fleet-dna.csv')
+df_vehicle_table1 = fleet_data[['vid', 'vehicle_class', 'vocation', 'vehicel_type', 'drivetrain_type', 'fuel_type' ,'day_id', 'driving_data_duration_hrs_no_zero', 'max_speed', 'driving_average_speed_no_zero', 'seconds_at_speed_zero', 'driving_time_seconds', 'percent_time_at_speed_zero', 'total_number_of_acceleration_events', 'total_number_of_deceleration_events', 'acceleration_events_per_mile', 'deceleration_events_per_mile', 'average_acceleration_event_duration', 'average_deceleration_event_duration', 'max_elevation', 'min_elevation', 'mean_elevation', 'delta_elevation', 'total_elevation_gained', 'total_elevation_lost', 'average_descending_rate']].copy()
+df_vehicle_table2 = fleet_data[['vid']].copy()
+
+#rounded data
+df_vehicle_table1 = df_vehicle_table1.round(decimals=2)
+
+tab_2_layout = html.Div([
+    html.H1('Page 2'),
+    html.Div(dash_table.DataTable(
+            id='table-2',
+            data=fleet_data.to_dict('records'),
+            columns=[{'name': i, 'id': i} for i in df_vehicle_table2.loc[:]
+                     ],
+            page_current=0,
+            page_size=20,
+            page_action='custom',
+
+            filter_action='custom',
+            filter_query='',
+
+            sort_action='custom',
+            sort_mode='multi',
+            sort_by=[],
+
+            style_cell={'textAlign': 'left'},
+            style_cell_conditional=[
+
+            ]),
+            style={'width': '20%', 'display': 'inline-block', 'overflowX': 'scroll'}),
+])
+
+#filter table 2
+
+operators = [['ge ', '>='],
+             ['le ', '<='],
+             ['lt ', '<'],
+             ['gt ', '>'],
+             ['ne ', '!='],
+             ['eq ', '='],
+             ['contains '],
+             ['datestartswith ']]
+
+def split_filter_part(filter_part):
+    for operator_type in operators:
+        for operator in operator_type:
+            if operator in filter_part:
+                name_part, value_part = filter_part.split(operator, 1)
+                name = name_part[name_part.find('{') + 1: name_part.rfind('}')]
+
+                value_part = value_part.strip()
+                v0 = value_part[0]
+                if (v0 == value_part[-1] and v0 in ("'", '"', '`')):
+                    value = value_part[1: -1].replace('\\' + v0, v0)
+                else:
+                    try:
+                        value = float(value_part)
+                    except ValueError:
+                        value = value_part
+
+
+                return name, operator_type[0].strip(), value
+
+    return [None] * 3
+
