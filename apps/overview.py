9ee36f189df4f76037569de6ac2cdb94478020da
ts170 <ts170@hdm-stuttgart.de>
2020-06-18 16:42:27 +0200

    implement Database connection

--- apps/overview.py
+++ apps/overview.py
@@ -1,4 +1,14 @@
+import pandas as pd
 import dash_html_components as html
+from database_connection import connect
+import dash_table
+
+# connect to database and add files to
+conn = connect()
+sql = "select * from cleaned_data_fleet_dna;"
+df = pd.read_sql_query(sql, conn)
+df = df.head(10)
+conn = None
 
 layout = html.Div(children=[
     html.H1(children='Overview'),
@@ -8,4 +18,11 @@ layout = html.Div(children=[
                       'laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in '
                       'voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat '
                       'non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'),
+
+    html.Div(dash_table.DataTable(
+                id='table-2',
+                data=df.to_dict('records'),
+                columns=[{'name': i, 'id': i} for i in df.loc[:]
+                         ],
+    ),)
 ])
