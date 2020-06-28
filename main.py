6c1eb491668483a07693f0714eb4b71459d13858
JohannesStroebele <stroebele.j@gmail.com>
2020-06-29 00:36:54 +0200

    Added __init__.py for recognizing modules

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import flask
-import sys
-sys.path.append('/apps')
+# import sys
+# sys.path.append('/apps')
 from apps import vehiclestables, downtimes, controlling, overview
 
 
