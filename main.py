b98d5cb474e0dc601ab05f95a1d33d991de1d7e4
JohannesStroebele <stroebele.j@gmail.com>
2020-06-29 11:37:02 +0200

    Changed app yaml application for deploying

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import flask
-# import sys
-# sys.path.append('/apps')
 from apps import vehiclestables, downtimes, controlling, overview
 
 
